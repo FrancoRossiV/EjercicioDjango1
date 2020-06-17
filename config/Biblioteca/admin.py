127ed74e2366161ae9af9dd9850f333bb2f3b0bb
Franco Rossi Villalva <FrancoRossiV>
2020-06-17 20:33:18 -0300

    Probando un poco mas la personalizacion del admin en Usuarios

--- config/Biblioteca/admin.py
+++ config/Biblioteca/admin.py
@@ -5,8 +5,21 @@ from Biblioteca.models import Libro
 from Biblioteca.models import Ejemplar
 from Biblioteca.models import Usuario
 
+
+class UsuarioAdmin(admin.ModelAdmin):
+
+	fieldsets = (
+		('Informacion Personal', {
+			'fields': ('Nombre', 'Direccion', 'Telefono')
+			}),
+		(None ,{
+			'fields': ()
+			})
+		)
+
+
 # Register your models here.
 admin.site.register(Autor,)
 admin.site.register(Libro,)
 admin.site.register(Ejemplar,)
-admin.site.register(Usuario,)
+admin.site.register(Usuario, UsuarioAdmin)
